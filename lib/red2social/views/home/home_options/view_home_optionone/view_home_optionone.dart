19b659cb479a8edbfe03d6e6f7e45dad9fcc514f
Alfredo alvarado <alfrellack@gmail.com>
2023-11-19 17:38:05 -0500

    modified:   lib/red2social/views/home/home_options/view_home_optionone/view_home_optionone.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/red2social/views/home/home_options/view_home_optionone/view_home_optionone.dart
+++ lib/red2social/views/home/home_options/view_home_optionone/view_home_optionone.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class ViewHomeOptionone extends StatelessWidget {
-  const ViewHomeOptionone({super.key});
+  const ViewHomeOptionone({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: const EdgeInsets.only(top: 30),
+        width: 400,
+        height: 900,
+        margin: const EdgeInsets.only(top: 30, left: 20, right: 20, bottom: 20),
         padding: const EdgeInsets.all(5),
-        child: const Column(
-          children: [Text('data Home')],
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              actions: [
+                Container(
+                  alignment: Alignment.center,
+                  width: 300,
+                  child: const Text('data'),
+                )
+              ],
+            ),
+            const MaxGap(10),
+            SliverAnimatedGrid(
+              initialItemCount: 10,
+              itemBuilder: (context, index, animation) {
+                return ScaleTransition(
+                  scale: animation,
+                  child: Container(
+                    alignment: Alignment.center,
+                    color: const Color.fromRGBO(68, 138, 255, 1),
+                    child: SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Image.asset('assets/splash.png'),
+                    ),
+                  ),
+                );
+              },
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 10.0,
+                mainAxisSpacing: 10.0,
+                childAspectRatio: 1.0,
+              ),
+            ),
+          ],
         ),
       ),
     );
